aa48737473ea791f525a9f8c38c2d8166c2e9d98
siloneco <siloneco4646@gmail.com>
2023-08-09 08:55:48 +0900

    feat: Add nav bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
-// import './globals.css'
 import { Inter } from 'next/font/google'
+import HeaderNav from '@/components/layout/HeaderNav'
+import resetStyle from '@/components/style/Reset.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body style={{ backgroundColor: "#f5f5f5" }} className={inter.className}>{children}</body>
+      <body style={{ backgroundColor: "#f5f5f5" }} className={`${inter.className} ${resetStyle.reset}`}>
+        <HeaderNav />
+        <div>{children}</div>
+      </body>
     </html>
   )
 }
---
+++ src/components/layout/HeaderNav/index.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import styles from "./style.module.css"
+import linkStyle from "@/components/style/LinkStyle.module.css"
+import { FaHome } from 'react-icons/fa';
+
+export default function HeaderNav() {
+    return (
+        <nav className={styles.navBackground}>
+            <div className={styles.navContent}>
+                <Link href={"/"} className={`${linkStyle.linkWithoutStyle} ${styles.navLinks}`}><FaHome style={{ fontSize: "1.5rem", paddingRight: "5px" }} />Home</Link>
+            </div>
+        </nav>
+    )
+}
