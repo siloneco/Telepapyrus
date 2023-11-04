d6b1c364cae5bbbe21314f72b18196a46717fcbc
siloneco <siloneco4646@gmail.com>
2023-11-04 21:02:17 +0900

    perf: surround with time tag for post time

--- src/components/article/ArticleCard/index.tsx
+++ src/components/article/ArticleCard/index.tsx
@@ -14,6 +14,9 @@ type Props = {
 }
 
 export default function ArticleCard({ id, title, date, lastUpdated, tags }: Props) {
+    const dateTime = date.replace(/\//g, '-')
+    const lastUpdatedDateTime = (lastUpdated !== undefined && lastUpdated !== null) ? lastUpdated?.replace(/\//g, '-') : ''
+
     return (
         <div className={styles.card}>
             <Link href={`/post/${id}`} className={linkStyle.linkWithoutStyle}>
@@ -22,9 +25,10 @@ export default function ArticleCard({ id, title, date, lastUpdated, tags }: Prop
             <div>
                 <div className={articleStyle.dateContainer}>
                     <p className={articleStyle.date}>
-                        <FaRegClock className={articleStyle.dateIcon} />{date}
+                        <FaRegClock className={articleStyle.dateIcon} />
+                        <time dateTime={dateTime}>{date}</time>
                     </p>
-                    {lastUpdated && <p className={articleStyle.date}>(最終更新: {lastUpdated})</p>}
+                    {lastUpdated && <p className={articleStyle.date}>(最終更新: <time dateTime={lastUpdatedDateTime}>{lastUpdated}</time>)</p>}
                 </div>
                 <div className={articleStyle.tags}>
                     {tags.map((tag) => <ArticleTag key={tag} tag={tag} />)}
--- src/components/article/ArticleHeader/index.tsx
+++ src/components/article/ArticleHeader/index.tsx
@@ -10,15 +10,19 @@ type Props = {
 }
 
 export default function ArticleHeader({ title, date, lastUpdated, tags }: Props) {
+    const dateTime = date.replace(/\//g, '-')
+    const lastUpdatedDateTime = (lastUpdated !== undefined && lastUpdated !== null) ? lastUpdated?.replace(/\//g, '-') : ''
+
     return (
         <div>
             <h1 className={articleStyle.title}>{title}</h1>
             <div>
                 <div className={articleStyle.dateContainer}>
                     <p className={articleStyle.date} >
-                        <FaRegClock className={articleStyle.dateIcon} />{date}
+                        <FaRegClock className={articleStyle.dateIcon} />
+                        <time dateTime={dateTime}>{date}</time>
                     </p>
-                    {lastUpdated && <p className={articleStyle.date}>(最終更新: {lastUpdated})</p>}
+                    {lastUpdated && <p className={articleStyle.date}>(最終更新: <time dateTime={lastUpdatedDateTime}>{lastUpdated}</time>)</p>}
                 </div>
                 <div className={articleStyle.tags}>
                     {tags.map((tag) => <ArticleTag key={tag} tag={tag} />)}
