1aece22a2cbad855ef0869d9556ed76ff092ae6e
siloneco <siloneco4646@gmail.com>
2023-11-28 14:35:12 +0900

    feat: error handling on draft preview

--- src/components/article/DraftPreview/index.tsx
+++ src/components/article/DraftPreview/index.tsx
@@ -3,6 +3,30 @@
 import { useRouter } from 'next/navigation'
 import { useContext, useEffect, useState, useTransition } from 'react'
 import { TabContext } from '../DraftWorkspace'
+import { ErrorBoundary } from 'react-error-boundary'
+import { Button } from '@/components/ui/button'
+
+function fallbackRender({ error, resetErrorBoundary }: any) {
+  return (
+    <div className="w-full">
+      <div className="w-fit mx-auto">
+        <p>
+          Failed to load preview. Digest:{' '}
+          <span className="text-red-400">{error.digest}</span>
+        </p>
+      </div>
+      <div className="w-fit mx-auto mt-4">
+        <Button
+          variant={'secondary'}
+          onClick={resetErrorBoundary}
+          className="text-base"
+        >
+          Reload
+        </Button>
+      </div>
+    </div>
+  )
+}
 
 type Props = {
   children: React.ReactNode
@@ -35,5 +59,7 @@ export default function DraftPreview({ children }: Props) {
     return null
   }
 
-  return <>{children}</>
+  return (
+    <ErrorBoundary fallbackRender={fallbackRender}>{children}</ErrorBoundary>
+  )
 }
