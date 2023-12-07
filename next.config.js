bcf8b5640abdbc0cd53d372fd3c86e9c00cbff05
siloneco <siloneco4646@gmail.com>
2023-12-07 20:57:45 +0900

    docs: add annotation for maintenanceability in future

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Required for Rehype Pretty Code to work.
+  // See: https://github.com/atomiks/rehype-pretty-code/issues/128 and https://github.com/siloneco/Telepapyrus/pull/192
   webpack: (config) => {
     config.module.rules.push({
       test: /\.mjs$/,
