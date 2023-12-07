083ca956d4617f2e0deee2754fb5181798881a7f
siloneco <siloneco4646@gmail.com>
2023-12-07 20:31:07 +0900

    fix: build fails due to webpack strange behaviour related to mjs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: 'javascript/auto',
+    })
+    return config
+  },
+}
 
 module.exports = nextConfig
