68027d9b46e422da1d2b3a0bb780cc8ef57cba0a
siloneco <siloneco4646@gmail.com>
2023-12-10 20:47:44 +0900

    feat: add warning in dashboard when invalid id is specified

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function isValidID(id: string) {
+  // check if id is alphanumeric + hiphen
+  return /^[a-z0-9-]+$/i.test(id)
+}
