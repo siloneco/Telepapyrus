f19929ce38f0b9b1c14d5b0a7e434fef73b025e1
siloneco <siloneco4646@gmail.com>
2024-01-06 23:35:31 +0900

    fix: browser console error on dev build due to changing aria-label for toggle theme button

--- src/components/misc/ToggleThemeButton/index.tsx
+++ src/components/misc/ToggleThemeButton/index.tsx
@@ -19,15 +19,12 @@ export default function ToggleThemeButton({ className }: Props) {
     }
   }
 
-  const ariaLabel =
-    theme === 'dark' ? 'Switch to light mode' : 'Switch to dark mode'
-
   return (
     <Button
       variant={'outline'}
       onClick={onClick}
       className={cn('h-8 w-8 bg-card', className)}
-      aria-label={ariaLabel}
+      aria-label="Toggle theme"
     >
       <Sun className="absolute h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
