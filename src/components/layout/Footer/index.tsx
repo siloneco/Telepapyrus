d7505b8e24e6377c2411825028c79e081bbf2c61
siloneco <siloneco4646@gmail.com>
2024-01-13 15:29:29 +0900

    feat: add contact information for article content

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -1,11 +1,34 @@
+import CopyButton from '@/components/misc/CopyButton'
 import { Separator } from '@/components/ui/separator'
+import { cn } from '@/lib/utils'
+
+const textClassName = 'text-center text-foreground dark:text-foreground/60'
+
+const contactEmail = process.env.CONTACT_EMAIL
 
 export default function Footer() {
   return (
     <footer className="max-w-3xl mx-auto mt-3 mb-6">
       <Separator />
       <div className="mt-3">
-        <p className="text-center text-foreground dark:text-foreground/60">
+        {contactEmail && (
+          <p className={cn(textClassName, 'mb-4')}>
+            <span>記事の内容に関する指摘等は</span>
+            <a
+              href={`mailto:${contactEmail}`}
+              className="ml-2 text-link underline"
+            >
+              {contactEmail}
+            </a>
+            <CopyButton
+              value={contactEmail}
+              className="h-6 w-6 align-bottom"
+              hoverAccent={false}
+            />
+            <span>まで</span>
+          </p>
+        )}
+        <p className={textClassName}>
           Contribute at GitHub:{' '}
           <a
             href="https://github.com/siloneco/Telepapyrus"
