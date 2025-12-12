6a7d73249486b2c073cfb0a431e429d7dd0503e3
siloneco <siloneco4646@gmail.com>
2025-12-12 12:07:57 +0000

    fix: lint errors

--- src/components/ui/use-toast.ts
+++ src/components/ui/use-toast.ts
@@ -14,7 +14,7 @@ type ToasterToast = ToastProps & {
   action?: ToastActionElement
 }
 
-const actionTypes = {
+const _actionTypes = {
   ADD_TOAST: 'ADD_TOAST',
   UPDATE_TOAST: 'UPDATE_TOAST',
   DISMISS_TOAST: 'DISMISS_TOAST',
